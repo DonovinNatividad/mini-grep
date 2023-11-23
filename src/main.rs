335f2145e17a00effdb140c80150e8dd82830150
Donovin Natividad <donovincall@gmail.com>
2023-11-23 00:48:36 -0500

    removing functionality from main and into run fn

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,26 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config: Config = Config::build(&args).unwrap_or_else(|err: &str| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
-    })
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
+    run(config);
+
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.file_path).expect( "Should have been able to read the file");
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
+
 
 struct Config {
     query: String,
